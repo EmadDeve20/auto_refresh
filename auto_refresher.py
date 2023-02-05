acab6e073d340d55e0186a5529e238374046c45d
EmadDeve20 <EmadDeve20@gmail.com>
2023-02-05 11:01:48 +0330

    add minimum app with big bug :)

--- auto_refresher.py
+++ auto_refresher.py
@@ -1,12 +1,33 @@
 from PyQt5 import QtWidgets, uic
 from PyQt5 import QtCore
+from selenium import webdriver
 import sys
+import time
 
 class AutoRefresher(QtWidgets.QMainWindow):
     def __init__(self):
         super(AutoRefresher, self).__init__()
         uic.loadUi("./autorefresher.ui", self)
 
+        self.startBtn.clicked.connect(self.run)
+    
+    def run(self):
+
+        url = self.urlLineEdit.text()
+        web_driver =  self.browsers[self.browsersCombo.currentIndex()]()
+        period = int(self.periodEdt.text())
+
+        while True:
+            web_driver.get(url)
+            time.sleep(period)
+
+    @property
+    def browsers(self):
+        return [
+            webdriver.Chrome,
+            webdriver.Firefox,
+            webdriver.Ie,
+        ]
 
 
 if __name__ == "__main__":
